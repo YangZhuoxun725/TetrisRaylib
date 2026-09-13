e9fe87a8175f34b9cc5e0e80f591be205f29fa86
YangZhuoxun725 <yangzhuoxun1234@gmail.com>
2026-09-13 16:29:05 +0800

    Initialize Premake

---
+++ premake5.lua
@@ -0,0 +1,62 @@
+workspace "TetrisRaylib"
+    architecture "x64"
+    startproject "Tetris"
+
+    configurations
+    {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+IncludeDir = {}
+
+project "Tetris"
+    location "Tetris"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++17"
+    staticruntime "On"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp",
+    }
+
+    includedirs
+    {
+        "%{prj.name}/src"
+    }
+
+    links
+    {
+
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+        buildoptions { "/utf-8" }
+
+        defines
+        {
+
+        }
+
+    filter "configurations:Debug"
+        symbols "On"
+        optimize "Debug"
+
+    filter "configurations:Release"
+        symbols "Off"
+        optimize "On"
+
+    filter "configurations:Dist"
+        symbols "Off"
+        optimize "Speed"
+    
